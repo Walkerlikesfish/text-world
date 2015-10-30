247ac4931893cf3d03b1f398604d99128edc3f99
Karthik <nkartr@gmail.com>
2015-10-30 09:07:15 -0400

    .

--- evennia/stop.sh
+++ evennia/stop.sh
@@ -1,1 +1,6 @@
-cd game && evennia stop && cd ..
+#USAGE: ./stop.sh <num_of_parallel_game_servers>
+
+NUM_GAMES=$1
+for ((i = 1; i <= $NUM_GAMES; i++ )); do
+    cd game$i && evennia stop && cd ..
+done;
